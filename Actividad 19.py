a6762ee6360900872b742f878e8056ea1b4456e5
Jackelin <jackelinvasquezguzman@gmail.com>
2025-08-13 20:12:57 -0600

    Modificación

--- Actividad 19.py
+++ Actividad 19.py
@@ -1,5 +1,6 @@
 Galletas = []  #Aqui se guardaran las galletas :)
 
+#Clase base "Galleta"
 class Galleta:
     def __init__(self, nombre, precio, peso):
         if not nombre:
@@ -18,6 +19,7 @@ class Galleta:
     def mostrar_informacion(self):
         print(f"Nombre:{self.nombre}-Precio:{self.precio}-Peso:{self.peso}")
 
+#Esta clase es "Hija" de la glase galleta
 class GalletaChispas(Galleta):
     def __init__(self,nombre,precio,peso,canitdad_chispas):
         super().__init__(nombre,precio,peso)
@@ -35,6 +37,7 @@ class Relleno:
     def describir_relleno(self):
         return f"relleno de {self.sabor_relleno}"
 
+#Esta clase hereda de clase galleta y clase relleno
 class GalletaRellena(Galleta,Relleno):
     def __init__(self,nombre,precio,peso,sabor_relleno):
         Galleta.__init__(self,nombre,precio,peso)
@@ -42,6 +45,12 @@ class GalletaRellena(Galleta,Relleno):
 
     def mostrar_informacion(self):
         print(f"Nombre:{self.nombre}-Precio:{self.precio}-Peso:{self.peso}-Relleno:{self.describir_relleno()}")
+
+#Esta clase es para la Excepcion Personalizada
+class RegistroDuplicadoError(Exception):
+    def __init__(self,mensaje):
+        self.mensaje= mensaje
+        super().__init__(mensaje)
 
 def menu():
     print(f"---Menú---\n1.Registrar Galleta Básica\n2.Registrar Galleta con Chispas\n3.Registrar Gallera Rellena")
@@ -60,31 +69,27 @@ def numero(mensaje):
 
 #Funcion opción 1 para registrar galletas
 def registrar_galleta():
-    nombre= input("Ingrese nombre de gallera")
-    encontrado= False
-    for galleta in Galletas:
-        if galleta.nombre.lower() == nombre.lower():
-            print(f"Galleta {nombre} ya existe")
-            encontrado= True
-            break
-    if not encontrado:
+    try:
+        nombre= input("Ingrese nombre de gallera")
+        for galleta in Galletas:
+            if galleta.nombre.lower() == nombre.lower(): # se verifica que la galleta este o no en la lista
+                raise RegistroDuplicadoError("Error. El nombre ya existe...")
         precio= numero("Ingrese precio de gelleta:")
         peso= numero(("Ingrese peso de galleta:"))
         nueva_galleta= Galleta(nombre,precio,peso)
         Galletas.append(nueva_galleta)
         print(f"Galleta {nombre} registrada de forma existosa!")
-
+    except RegistroDuplicadoError as e:
+        print("Error:",e)
 
 #Funcion par aopcion 2
 def registrar_galleta_chispas():
-    encontrado= False
-    galleta_chispas= input("Ingrese nombre de galleta con chispas:")
-    for galleta in Galletas:
-        if galleta.nombre.lower() == galleta_chispas.lower():
-            print("Galleta ya resgitrada...")
-            encontrado= True
-            break
-    if not encontrado:
+    try:
+        galleta_chispas= input("Ingrese nombre de galleta con chispas:")
+        for galleta in Galletas:
+            if galleta.nombre.lower() == galleta_chispas.lower():
+                raise RegistroDuplicadoError("Error.El nombre ya existe...")
+
         precio= numero("Ingrese precio de galleta con chispas:")
         peso= numero("Ingrese preso de galleta con chispas:")
         while True:
@@ -98,23 +103,26 @@ def registrar_galleta_chispas():
                 print("Error. Ingrese un número entero positivo...")
         nueva_galleta= GalletaChispas(galleta_chispas,precio,peso,cantida_chispas)
         Galletas.append(nueva_galleta)
+    except RegistroDuplicadoError as e:
+        print("Error:", e)
 
+#funcion opcion 3 - registro galleta con relleno
 def registrar_galleta_rellena():
-    nombre= input("Ingrese nombre de galleta rellena:")
-    encontrado= False
-    for galleta in Galletas:
-        if galleta.nombre.lower() == nombre.lower():
-            print("Galleta ya resgitrada...")
-            encontrado= True
-            break
+    try:
+        nombre= input("Ingrese nombre de galleta rellena:")
+        for galleta in Galletas:
+            if galleta.nombre.lower() == nombre.lower():
+                raise RegistroDuplicadoError(f"Error.{nombre} ya se encuentra registrado...")
 
-    if not encontrado:
         precio= numero("Ingrese precio de galleta rellena:")
         peso= numero("Ingrese peso de galletra rellena:")
         relleno= input("Ingrese el relleno:")
         nueva_galleta= GalletaRellena(nombre,precio,peso,relleno)
         Galletas.append(nueva_galleta)
+    except RegistroDuplicadoError as e:
+        print("Error:", e)
 
+#funcion opcion 4 listado de galletas
 def lista_galletas():
     if not Galletas:
         print("Lista de galletas vacia :(")
@@ -124,6 +132,7 @@ def lista_galletas():
             galleta.mostrar_informacion()
             print("---"*4)
 
+#Funcion opcion 5- busqueda de galleta por nombre
 def bucar_galleta_nombre():
     nombre= input("Ingrese nombre a buscar:")
     for galleta in Galletas:
@@ -134,6 +143,7 @@ def bucar_galleta_nombre():
     else:
         print(f"No se ha encontrado la galleta {nombre}...")
 
+#funcion 6. Eliminar registro de galleta pro nombre
 def eliminar_nombre():
     nombre= input("Ingrese nombre de galleta a eliminar:")
     for galleta in Galletas:
